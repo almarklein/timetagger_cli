7e37ec99be87660bb12aa0fd10953eb25421d810
Almar Klein <almar@almarklein.org>
2024-07-26 11:16:24 +0200

    Bump version to 24.7.1

--- timetagger_cli/__init__.py
+++ timetagger_cli/__init__.py
@@ -2,7 +2,7 @@
 Track your time from the command-line, a CLI for https://timetagger.app.
 """
 
-__version__ = "23.8.3"
+__version__ = "24.7.1"
 
 version_info = tuple(map(int, __version__.split(".")))
 
